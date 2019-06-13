5a77429fe95dc1d20c88af436e7a2c014080649b
skycho71 <skycho71@gmail.com>
2019-06-13 09:20:20 +0900

    edit

--- lecture11(6.12)/lecture/1-8.js
+++ lecture11(6.12)/lecture/1-8.js
@@ -21,22 +21,19 @@ server.on('request', function(req, res) {
 
     res.writeHead(200, {"Content-Type": "image/png"});
 
-infile.on('readable', function() {
-    var chunk;
-    while (null !== (chunk = infile.read())) {
-        console.log('읽어 들인 데이터 크기 : %d 바이트', chunk.length);
-        curlength += chunk.length;
-        res.write(chunk, 'utf8', function(err) {
-            console.log('파일 부분 쓰기 완료 : %d, 파일 크기 : %d', curlength, filelength);
-            if (curlength >= filelength) {
-                res.end();
-            }
-        });
-    }
-});	 
-
-
-	 
+    infile.on('readable', function() {
+        var chunk;
+        while (null !== (chunk = infile.read())) {
+            console.log('읽어 들인 데이터 크기 : %d 바이트', chunk.length);
+            curlength += chunk.length;
+            res.write(chunk, 'utf8', function(err) {
+                console.log('파일 부분 쓰기 완료 : %d, 파일 크기 : %d', curlength, filelength);
+                if (curlength >= filelength) {
+                    res.end();
+                }
+            });
+        }
+    });	 
 });
 
 
